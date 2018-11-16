1444cfa2cae36ebb076c42e3fcc048b7a7cef330
alexluca97 <alexluca97@yahoo.com>
2018-11-16 08:47:15 +0200

    add ShoppingCart Class

--- Laborator7/Laborator7.Domain/Product/Product.cs
+++ Laborator7/Laborator7.Domain/Product/Product.cs
@@ -6,5 +6,10 @@ namespace Laborator7.Domain.Product
 {
     class Product
     {
+        public Guid id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Pieces { get; set; }
+        public double Total { get; set; }
     }
 }
--- Laborator7/Laborator7.Domain/ShoppingCart/ShoppingCart.cs
+++ Laborator7/Laborator7.Domain/ShoppingCart/ShoppingCart.cs
@@ -6,5 +6,9 @@ namespace Laborator7.Domain.ShoppingCart
 {
     class ShoppingCart
     {
+        public Guid id { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public int Total { get; set; }
     }
 }
